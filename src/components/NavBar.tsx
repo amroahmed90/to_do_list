4c229d1b346a48dd853386b772b06cd2e1bd47dc
Amro Abdelfatah <amro.utp.pe@gmail.com>
2024-05-12 14:30:46 +0200

    chore(mui): add material UI to deps

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { makeStyles } from "tss-react/mui";
+import Paper from "@mui/material/Paper";
+
+const useStyles = makeStyles()((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.default,
+  },
+}));
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const { classes } = useStyles();
+
+  return <Paper className={classes.root}>NavBar</Paper>;
 };
 
 export default NavBar;
